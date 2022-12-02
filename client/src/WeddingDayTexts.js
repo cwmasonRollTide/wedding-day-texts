6e6a556cfeb5888088d1d0fdac78225205e86728
Connor Mason <cdubyamason@outlook.com>
2022-12-02 05:16:18 -0500

    make this site build online

--- client/src/WeddingDayTexts.js
+++ client/src/WeddingDayTexts.js
@@ -1,17 +1,11 @@
-import S3Client from "./api/s3client";
+import * as React from 'react';
 import {useEffect, useState} from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import { useTheme, ThemeProvider } from '@mui/material/styles';
 import MessageDisplay from "./components/messagedisplay/messageDisplay";
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import {useStyles} from "./style";
 
-const s3Bucket = new S3Client(process?.env?.AWS_REGION ?? "us-east-1");
+// const s3Bucket = new S3Client(process?.env?.AWS_REGION ?? "us-east-1");
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
@@ -21,9 +15,9 @@ const WeddingDayTexts = () => {
     const [messages, setMessages] = useState([]);
     
     useEffect(async () => {
-        const messages = await s3Bucket.getMessages();
+        // const messages = await s3Bucket.getMessages();
         //TODO: translate bucket response into same shape as what comes into incoming queue
-        setMessages(messages);
+        setMessages([]);
     }, [])
 
     return (
