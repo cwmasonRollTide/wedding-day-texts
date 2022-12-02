d5ebba867ea66221efba6294bbde5de92e0b5eec
Connor Mason <cdubyamason@outlook.com>
2022-12-02 04:54:42 -0500

    blah

--- client/src/WeddingDayTexts.js
+++ client/src/WeddingDayTexts.js
@@ -2,8 +2,7 @@ import S3Client from "./api/s3client";
 import {useEffect, useState} from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import MessageList from "./components/messagelist/messagelist";
-const s3Bucket = new S3Client(process?.env?.AWS_REGION ?? "us-east-1");
+import MessageDisplay from "./components/messagedisplay/messageDisplay";
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
@@ -11,6 +10,8 @@ import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import {useStyles} from "./style";
+
+const s3Bucket = new S3Client(process?.env?.AWS_REGION ?? "us-east-1");
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
@@ -30,7 +31,7 @@ const WeddingDayTexts = () => {
             <ThemeProvider theme={theme}>
                 <div role="App">
                     <Header />
-                    <MessageList messages={messages} />
+                    <MessageDisplay messages={messages} />
                     <Footer />
                 </div>
             </ThemeProvider>
