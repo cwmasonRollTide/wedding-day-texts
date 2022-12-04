d04c1165b6e2b6e58146ecd56bc4c7ff81ac7f76
Connor Mason <cdubyamason@outlook.com>
2022-12-03 22:04:09 -0500

    front end tests

--- client/src/components/messagedisplay/messageDisplay.js
+++ client/src/components/messagedisplay/messageDisplay.js
@@ -1,43 +1,48 @@
-import React, {useEffect, useState} from 'react';
 import { useStyles } from "./style";
 import PropTypes from "prop-types";
 import TextPost from "../textpost/textpost";
 import ImagePost from "../imagepost/imagepost";
-import {S3Client} from "@aws-sdk/client-s3";
-import StorageClient from "../../api/storageclient";
+import React, {useEffect, useState} from 'react';
+import S3ClientImplementation from "../../api/s3client";
 
 export const MessageDisplay = () => {
     const classes = useStyles();
     const [messages, setMessages] = useState([]);
-    const s3Bucket = new S3Client({region: "us-east-2" });
-    const storageClient = new StorageClient(s3Bucket);
+    const s3Bucket = new S3ClientImplementation("us-east-2");
 
-    useEffect(async () => {
+    useEffect(() => {
         try {
-            const messages = await storageClient.getMessages();
-            setMessages(messages);
+            s3Bucket.getMessages()
+                .then(messages =>
+                    setMessages(messages))
+                .catch(err => {
+                    console.log(err);
+                });    
         } catch (err) {
-            console.log("BLASH AL:OQIJKFN");
             console.log(err);
         }
-    }, [])
+    }, []);
+    
+    //TODO: add topic for message safe notification to also push to with new message payloads coming in,
+    //so the only time we call the s3 bucket that holds the safe messages is on first render
 
     // <summary>
     // For now, we are just handling SMS and MMS, so if the numMedia field is greater than zero, it is an image message 
     // </summary
     const renderMessage = (data) => {
-      if (data?.numMedia > 0) 
+      if (data?.numMedia > 0 && data?.image) 
           return <ImagePost message={data} />;
       return <TextPost message={data?.messageBody} />;
     };
+    
     return (
         <div data-testid="messagecontainer" id="images" className={classes.container}>
             <h1>WEDDING DAY TEXTS</h1>
-            <div className="row">
-                {messages?.map((data, index) => {
-                    renderMessage(data);
-                })}
-            </div>
+            {messages?.map((data, index) => 
+                <div key={index} className={classes.messageEntry} data-testid="message-entry">
+                    { renderMessage(data) }
+                </div>
+            )}
         </div>
     );
 }
