a1cb6264e99fc389b2aa22ae2b76f53bcc6273c3
Connor Mason <cdubyamason@outlook.com>
2022-12-05 05:14:24 -0500

    going different route/starting over with arch on smaller scale at a time

--- client/src/pages/MainPage/style.js
+++ client/src/pages/MainPage/style.js
@@ -4,8 +4,8 @@ export const useStyles = makeStyles(theme => ({
     App: {
         height: '100%',
         display: 'flex',
-        // flexDirection: 'column',
-        // flexWrap: 'wrap',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
         // flex: 'auto',
         backgroundColor: 'lightgrey',
         justifyContent: 'stretch'
--- functions/queries/censorship/index.js
+++ functions/queries/censorship/index.js
@@ -1,4 +1,6 @@
+'use strict'
+
 exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return;
+  return '200';
 }
