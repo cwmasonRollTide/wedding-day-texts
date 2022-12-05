4c56474e7bd5a24581a2d9ccdc51c039e78775d6
Connor Mason <cdubyamason@outlook.com>
2022-12-04 23:28:43 -0500

    blah

--- client/src/components/footer/footer.test.js
+++ client/src/components/footer/footer.test.js
@@ -6,12 +6,6 @@ describe('Footer tests', () => {
     it('Should render footer', () => {
         const { getByRole } = customRender(<Footer />);
         expect(getByRole("footer")).not.toBeNull();
-        expect(getByRole("link-list")).not.toBeNull();
-    })
-    
-    it('Link should have attribute', () => {
-       const { getByTestId } = customRender(<Footer />);
-        expect(getByTestId('site-link')).toHaveAttribute('href', "https://www.theknot.com/us/anna-lynd-and-connor-mason-dec-2022");
     });
 });
 
