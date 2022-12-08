671ea909a8ac6896e4cb73aab3aaad448d05521d
Connor Mason <cdubyamason@outlook.com>
2022-12-08 18:18:52 -0500

    added code to call amazon rekognition and determine if image is bad. for now just logging what rekognition will actually return to us since I dont have any exmaple payloads to go off of

--- functions/queries/censorship/index.js
+++ functions/queries/censorship/index.js
@@ -2,6 +2,7 @@
 const https = require('https');
 const { Rekognition } = require('aws-sdk')
 
+// Use simple http client to retrieve the image data via get request
 function getImage(url) {
   return new Promise((resolve, reject) => {
     const req = https.get(url, res => {
@@ -26,12 +27,14 @@ function getImage(url) {
   });
 }
 
-function detectInnappropriateNonoContent(image) {
+// Calls the rekognition api to determine if content is inappropriate.
+// Returns true if content should NOT be viewed by the general public
+// Returns false if the content is perfectly fine.
+function isContentInappropriate(image) {
   return new Promise((resolve, reject) => {
-    const rekognition = new Rekognition({
-      region: 'us-east-2', // like us-east-1
-    });
-    try {
+      const rekognition = new Rekognition({
+        region: 'us-east-2', // like us-east-1
+      });
       const request = {
         "Image": {
           "Bytes": image
@@ -40,80 +43,42 @@ function detectInnappropriateNonoContent(image) {
       };
       rekognition.detectModerationLabels(request, (err, data) => {
         if (err) {
-          
+          console.log(err);
+          reject(new Error(err ?? ''));
         } else {
-          
+          const isInappropContent = determineBadContent(data);
+          resolve(isInappropContent);
         }
-      }
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    } catch (err) {
-      
-    }
-  })
+      });
+  });
+}
+
+// Lets see what format the data is returned in. I know it's a list of objects with tags and confidence 
+// scores but still I don't have examples to work with - seems like they should provide sample payloads.
+function determineBadContent(data) {
+  console.log('THIS IS WHAT IS RETURNED FROM REKOGNITION: \n' + JSON.stringify(data, null, 2));
+  return true;
 }
 
 exports.handler =  async function(event, context) {
-  const image = await getImage(event.mediaUrl0)
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  console.log("Media: \n" + JSON.stringify(image, null, 2))
+  try {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    console.log("Media: \n" + JSON.stringify(image, null, 2));
+    console.log("Context: \n" + JSON.stringify(context, null, 2));
+    const image = await getImage(event['mediaUrl0']);
+    const contentIsInappropriate = await isContentInappropriate(image);
+    if (contentIsInappropriate === true) {
+      console.log("CONTENT IS NOT APPROPRIATE FOR GRANDMOTHA EYES YALL");
+      // Enqueue into profanity queue
+    } else {
+      console.log("CONTENT IS APPROPRIATE, DISPLAY AWAY");
+      // Enqueue into safe message queue
+    }
+  } catch (err) {
+    console.log('BIG ERROR');
+    console.log(err);
+    return { status: '500' };
+  }
+  
   return { status: '200' };
 }
