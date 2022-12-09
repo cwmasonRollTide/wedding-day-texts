5c468cc13a16189767af7f6020dac4dcd338433e
Connor Mason <cdubyamason@outlook.com>
2022-12-09 13:48:05 -0500

    oh shit this function is getting close. this should retrieve the image contents correctly, then send just a blob to the detectModerationLabels rekognition function

--- functions/queries/censorship/index.js
+++ functions/queries/censorship/index.js
@@ -1,41 +1,32 @@
 'use strict';
-const https = require('https');
 const AWS = require('aws-sdk');
+const fetch = require("node-fetch");
 
 // Use simple https client to retrieve the image data via get request
-function getImage(messageSid, accountSid) {
-  return new Promise((resolve, reject) => {
-    const req = https.get(url, res => {
-      let rawData = '';
-      res.on('data', chunk => {
-        rawData += chunk;
-      });
-      res.on('end', () => {
-        try {
-          console.log('Data:\n');
-          console.log(rawData);
-          resolve(rawData);
-        } catch (err) {
-          reject(new Error(err));
-        }
-      });
-    });
-    req.on('error', err => {
-      reject(new Error(err ?? ''));
-    });
+function getImage(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      console.log('BLOB: \n', null, 2, JSON.stringify(blob));
+      resolve(blob);
+    } catch (err) {
+      console.log(err);
+      reject(new Error(err ?? ''))
+    }
   });
 }
 
 // Calls the rekognition api to determine if content is inappropriate.
 // Returns true if content should NOT be viewed by the general public
 // Returns false if the content is perfectly fine.
-function isContentInappropriate(image) {
+function isContentInappropriate(imageBlob) {
   return new Promise((resolve, reject) => {
     // Initialize the Amazon Cognito credentials provider
     const rekognition = new AWS.Rekognition();
     const request = {
       "Image": {
-        "Bytes": image
+        "Bytes": imageBlob
       },
       "MinConfidence": 60
     };
@@ -55,30 +46,37 @@ function isContentInappropriate(image) {
 // scores but still I don't have examples to work with - seems like they should provide sample payloads.
 function determineBadContent(data) {
   console.log('THIS IS WHAT IS RETURNED FROM REKOGNITION: \n' + JSON.stringify(data, null, 2));
-  return data.ModerationLabels.any(x => 
+  return data?.ModerationLabels?.any(x => 
       x.Name.toLowerCase().contains('nudity') || 
       x.Name.toLowerCase().contains('middle finger') || 
-      x.Name.toLowerCase().contains('drug'));
+      x.Name.toLowerCase().contains('drug')) ?? false;
 }
 
 exports.handler =  async function(event, context) {
+  let allContentIsAppropriate = true;
   try {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    const image = await getImage(event.images[0]);
-    console.log("Media: \n" + JSON.stringify(image, null, 2));
-    const contentIsInappropriate = await isContentInappropriate(image);
-    if (contentIsInappropriate === true) {
-      console.log("CONTENT IS NOT APPROPRIATE FOR GRANDMOTHA EYES YALL");
-      // Enqueue into profanity queue
-    } else {
-      console.log("CONTENT IS APPROPRIATE, DISPLAY AWAY");
-      // Enqueue into safe message queue
+    event.images?.map(async (mediaUrl) => {
+      const image = await getImage(mediaUrl);
+      console.log("Media: \n" + JSON.stringify(image, null, 2));
+      const contentIsInappropriate = await isContentInappropriate(image);
+      if (contentIsInappropriate === true) {
+        console.log("CONTENT IS NOT APPROPRIATE FOR GRANDMOTHA EYES YALL");
+        allContentIsAppropriate = false;
+        // Enqueue into profanity queue
+      } else {
+        console.log("CONTENT IS APPROPRIATE, DISPLAY AWAY");
+        // Enqueue into safe message queue
+      }
+    });
+    if (allContentIsAppropriate === true) {
+      // Store the image file as png or jpeg, from number as the title to a txt document with the message body as the contents
+      console.log('All content approp');
     }
   } catch (err) {
     console.log('BIG ERROR');
     console.log(err);
     return { statusCode: '500' };
   }
-  
   return { statusCode: '200' };
 }
